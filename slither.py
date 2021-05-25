e6e268d07fff474bbcc165f61c2c8ddd669721bb
revathi727 <revathipolisetty7@gmail.com>
2021-05-25 22:54:05 +0530

    07 Moving Rectangle

--- slither.py
+++ slither.py
@@ -13,16 +13,21 @@ pygame.display.update()
 
 gameExit = False
 
+lead_x = 300
+lead_y = 300
+
 while not gameExit:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameExit = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                lead_x -= 10
+            if event.key == pygame.K_RIGHT:
+                lead_x += 10
 
     gameDisplay.fill(white)
-    pygame.draw.rect(gameDisplay, black, [400, 300, 10, 10])
-
-    gameDisplay.fill(red, rect=[200, 200, 50, 50])
-
+    pygame.draw.rect(gameDisplay, black, [lead_x, lead_y, 10, 10])
     pygame.display.update()
 
 pygame.quit()
