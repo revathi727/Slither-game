ae16c6d7364d845baeb1991c97b9873f38584fa7
revathi727 <revathipolisetty7@gmail.com>
2021-05-26 08:37:56 +0530

    11 Moving Up

--- slither.py
+++ slither.py
@@ -16,6 +16,7 @@ gameExit = False
 lead_x = 300
 lead_y = 300
 lead_x_change = 0
+lead_y_change = 0
 
 clock = pygame.time.Clock()
 
@@ -26,14 +27,19 @@ while not gameExit:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 lead_x_change = -10
-            if event.key == pygame.K_RIGHT:
+                lead_y_change = 0
+            elif event.key == pygame.K_RIGHT:
                 lead_x_change = 10
-
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                lead_y_change = 0
+            elif event.key == pygame.K_UP:
+                lead_y_change = -10
+                lead_x_change = 0
+            elif event.key == pygame.K_DOWN:
+                lead_y_change = 10
                 lead_x_change = 0
 
     lead_x += lead_x_change
+    lead_y += lead_y_change
     gameDisplay.fill(white)
     pygame.draw.rect(gameDisplay, black, [lead_x, lead_y, 10, 10])
     pygame.display.update()
