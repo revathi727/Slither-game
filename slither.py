37f3bd412294dbdf6f6250fec25229333d8c89f6
revathi727 <revathipolisetty7@gmail.com>
2021-05-26 08:53:20 +0530

    13 Fixing the Hardcoding

--- slither.py
+++ slither.py
@@ -6,19 +6,25 @@ white = (255,255,255)
 black = (0,0,0)
 red = (255,0,0)
 
-gameDisplay = pygame.display.set_mode((800, 600))
+display_width = 800
+display_height = 600
+
+gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('Slither')
 
 pygame.display.update()
 
 gameExit = False
 
-lead_x = 300
-lead_y = 300
+lead_x = display_width/2
+lead_y = display_height/2
 lead_x_change = 0
 lead_y_change = 0
 
 clock = pygame.time.Clock()
+
+block_size = 10
+FPS = 30
 
 while not gameExit:
     for event in pygame.event.get():
@@ -26,28 +32,28 @@ while not gameExit:
             gameExit = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                lead_x_change = -10
+                lead_x_change = -block_size
                 lead_y_change = 0
             elif event.key == pygame.K_RIGHT:
-                lead_x_change = 10
+                lead_x_change = block_size
                 lead_y_change = 0
             elif event.key == pygame.K_UP:
-                lead_y_change = -10
+                lead_y_change = -block_size
                 lead_x_change = 0
             elif event.key == pygame.K_DOWN:
-                lead_y_change = 10
+                lead_y_change = block_size
                 lead_x_change = 0
 
-    if lead_x>=800 or lead_x<0 or lead_y>=600 or lead_y<0:
+    if lead_x>=display_width or lead_x<0 or lead_y>=display_height or lead_y<0:
         gameExit = True
 
     lead_x += lead_x_change
     lead_y += lead_y_change
     gameDisplay.fill(white)
-    pygame.draw.rect(gameDisplay, black, [lead_x, lead_y, 10, 10])
+    pygame.draw.rect(gameDisplay, black, [lead_x, lead_y, block_size, block_size])
     pygame.display.update()
 
-    clock.tick(30)
+    clock.tick(FPS)
 
 pygame.quit()
 quit()
