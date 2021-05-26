837756fc47dc3f1c93db900a7723e94e3701c9a1
revathi727 <revathipolisetty7@gmail.com>
2021-05-26 12:08:49 +0530

    16 Adding an Apple

--- slither.py
+++ slither.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+import random
 
 pygame.init()
 
@@ -35,6 +36,8 @@ def gameLoop():
     lead_x_change = 0
     lead_y_change = 0
 
+    randAppleX = random.randrange(0, display_width-block_size)
+    randAppleY = random.randrange(0, display_height-block_size)
 
     while not gameExit:
         while gameOver == True:
@@ -73,6 +76,7 @@ def gameLoop():
         lead_x += lead_x_change
         lead_y += lead_y_change
         gameDisplay.fill(white)
+        pygame.draw.rect(gameDisplay, red, [randAppleX, randAppleY, block_size, block_size])
         pygame.draw.rect(gameDisplay, black, [lead_x, lead_y, block_size, block_size])
         pygame.display.update()
 
