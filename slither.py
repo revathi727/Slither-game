ee5fa5d7b612932505c9ac4f11e7f02081a642b5
revathi727 <revathipolisetty7@gmail.com>
2021-05-28 23:39:32 +0530

    24 Collision Detection

--- slither.py
+++ slither.py
@@ -109,17 +109,14 @@ def gameLoop():
 
         pygame.display.update()
 
-        # if lead_x==randAppleX and lead_y==randAppleY:
-        #     randAppleX = round(random.randrange(0, display_width-block_size)/10.0)*10.0
-        #     randAppleY = round(random.randrange(0, display_height-block_size)/10.0)*10.0
-        #     snakeLength += 1
 
+        # if lead_x>=randAppleX and lead_x<=randAppleX+AppleThickness:
+        #     if lead_y>=randAppleY and lead_y<=randAppleY+AppleThickness:
+        #             randAppleX = round(random.randrange(0, display_width-block_size))#/10.0)*10.0
+        #             randAppleY = round(random.randrange(0, display_height-block_size))#/10.0)*10.0
+        #             snakeLength += 1
 
-        if lead_x>=randAppleX and lead_x<=randAppleX+AppleThickness:
-            if lead_y>=randAppleY and lead_y<=randAppleY+AppleThickness:
-                    randAppleX = round(random.randrange(0, display_width-block_size))#/10.0)*10.0
-                    randAppleY = round(random.randrange(0, display_height-block_size))#/10.0)*10.0
-                    snakeLength += 1
+        if lead_x>randAppleX and lead_x<randAppleX+AppleThickness
 
         clock.tick(FPS)
 
