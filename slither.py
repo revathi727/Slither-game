0df63326dcf7d99775eb8132297d4e3c6fe8401d
revathi727 <revathipolisetty7@gmail.com>
2021-05-29 22:02:17 +0530

    31 Game Over Screen

--- slither.py
+++ slither.py
@@ -46,11 +46,11 @@ def text_objects(text, color):
     textSurface = font.render(text, True, color)
     return textSurface, textSurface.get_rect()
 
-def message_to_screen(msg, color):
+def message_to_screen(msg, color, y_displace=0):
     textSurf, textRect = text_objects(msg, color)
     # screen_text = font.render(msg, True, color)
     # gameDisplay.blit(screen_text, [display_width/2, display_height/2])
-    textRect.center = (display_width/2), (display_height/2)
+    textRect.center = (display_width/2), (display_height/2)+y_displace
     gameDisplay.blit(textSurf, textRect)
 
 def gameLoop():
@@ -72,7 +72,8 @@ def gameLoop():
     while not gameExit:
         while gameOver == True:
             gameDisplay.fill(white)
-            message_to_screen("Game over , press C to play again or Q to quit", red)
+            message_to_screen("Game over", red, -50)
+            message_to_screen("Press C to play again or Q to Quit", black, 50)
             pygame.display.update()
 
             for event in pygame.event.get():
