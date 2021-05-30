0ed1bd283e1fbfb6c949c353f1aa748ddd5a2b5b
revathi727 <revathipolisetty7@gmail.com>
2021-05-30 23:28:29 +0530

    41 Finishing Converting to Executable

--- setup.py
+++ setup.py
@@ -5,5 +5,7 @@ executables = [cx_Freeze.Executable("slither.py")]
 cx_Freeze.setup(
     name = "Slither",
     options = {"build_exe":{"packages":["pygame"], "include_files":["apple30px.png", "snakehead20px.png"]}},
-    
+
+    description = "Slither Game",
+    executables = executables
 )
